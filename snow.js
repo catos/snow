6d2d2b8ffddc2a3b6d6b6fd8412c52c81b063a7a
Cato Skogholt <cskogholt@gmail.com>
2018-12-04 22:14:45 +0100

    weee

--- snow.js
+++ snow.js
@@ -6,82 +6,153 @@
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
 
+    const fps = 30;
+    const fpsInterval = 1000 / fps;
+
+
     // GameState
-    let gs = {
+    let state = {
         snowflakes: [],
-        dt: 0,
-        dtAcc: 0,
+        start: 0,
+        elapsed: 0,
         prev: 0,
+        frame: 0,
+
+        colObjects: [
+            { x: 200, y: 200, w: 400, h: 400 },
+            { x: 500, y: 700, w: 300, h: 300 },
+        ]
     };
 
-    loop();
+    function init() {
+        // Start the loop
+        loop();
+    }
 
-    function snowFlake() {
-        gs.snowflakes.push({
+    init();
+
+    // Draw ground
+    function drawColObjects() {
+        ctx.beginPath();
+        ctx.fillStyle = `rgb(255, 255, 255, 0.5)`;
+        state.colObjects.map(p => {
+            ctx.fillRect(p.x, p.y, p.w, p.h);
+        });
+        ctx.closePath();
+    }
+
+    // Creates a snowflake
+    function snowflake() {
+        state.snowflakes.push({
             x: Math.floor(Math.random() * canvas.width),
             y: -10,
-            velocity: randomBetween(.25, .75),
-            opacity: randomBetween(.25, .75),
+            age: 0,
+            velocity: randomBetween(1, 3), // randomBetween(.25, .75),
+            opacity: randomBetween(.1, .8),
             radius: randomBetween(2, 4)
         });
     }
 
     function loop() {
-        const now = performance.now();
-        gs.dt = now - gs.prev;
-        gs.dtAcc = gs.dtAcc > 100 ? 0 : gs.dtAcc + gs.dt;
 
-        draw();
-        update();
-        debug();
-
-        gs.prev = now;
         requestAnimationFrame(loop);
+
+        const now = performance.now();
+        state.elapsed = now - state.prev;
+
+        if (state.elapsed > fpsInterval) {
+            draw();
+            update();
+            debug();
+
+            state.frame++;
+            state.prev = now;
+        }
+
     }
 
     function update() {
         // move snowflakes
-        gs.snowflakes = gs.snowflakes.map(p => {
+        state.snowflakes = state.snowflakes.map(p => {
             p.y += p.velocity;
-            p.x += Math.sin(p.y / 10) / 10;
+            if (p.velocity > 0) {
+                p.x += Math.sin(p.y / 25) / 2;
+            }
             return p;
         });
 
         // spawn new snowflakes
-        if (gs.dtAcc === 0) {
-            snowFlake();
+        if (state.frame % 2 === 0) {
+            snowflake();
         }
 
+        // check for collision
+        state.colObjects.forEach(co => {
+            state.snowflakes.forEach(sf => {
+                if (isColliding(co, sf)) {
+                    sf.velocity = 0;
+                }
+            });
+        });
+
+        // age snowflakes that has collided
+        state.snowflakes = state.snowflakes.map(p => {
+            if (p.velocity === 0) {
+                p.age++;
+            }
+            return p;
+        });
 
         // remove snowflakes out of screen
-        gs.snowflakes = gs.snowflakes.filter(p => p.y < canvas.height + 50);
+        state.snowflakes = state.snowflakes.filter(p => p.y < canvas.height + 50);
+        
+        // remove totally melted snowflakes
+        state.snowflakes = state.snowflakes.filter(p => p.age < 30 * 60 * 3);
     }
 
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        gs.snowflakes.forEach(p => {
+
+        // draw collidable objects
+        drawColObjects();
+
+        state.snowflakes.forEach(p => {
             ctx.beginPath();
             ctx.fillStyle = `rgb(255, 255, 255, ${p.opacity})`;
             ctx.arc(p.x, p.y, p.radius, 0, Math.PI * 2, true);
             ctx.fill();
             ctx.closePath();
-        })
+        });
     }
 
     function debug() {
         ctx.beginPath();
         ctx.font = '14px serif';
         ctx.fillStyle = `rgb(255, 255, 255, 1)`;
-        ctx.fillText(`flakes: ${gs.snowflakes.length}`, 20, 100);
-        ctx.fillText(`now: ${Math.floor(performance.now())}`, 20, 125);
-        ctx.fillText(`dt: ${Math.floor(gs.dt)}`, 20, 150);
-        ctx.fillText(`dtAcc: ${Math.floor(gs.dtAcc)}`, 20, 175);
-        ctx.fillText(`prev: ${Math.floor(gs.prev)}`, 20, 200);
+        ctx.fillText(`flakes: ${state.snowflakes.length}`, 20, 100);
+        ctx.fillText(`start: ${Math.floor(performance.now())}`, 20, 120);
+        ctx.fillText(`now: ${Math.floor(performance.now())}`, 20, 140);
+        ctx.fillText(`elapsed: ${Math.floor(state.elapsed)}`, 20, 160);
+        ctx.fillText(`prev: ${Math.floor(state.prev)}`, 20, 180);
+        ctx.fillText(`frame: ${Math.floor(state.frame)}`, 20, 200);
         ctx.closePath();
+    }
+
+    function isColliding(colObject, snowflake) {
+        if (snowflake.opacity < .3 || snowflake.opacity > .6) {
+            return false;
+        }
+
+        if (snowflake.x >= colObject.x && snowflake.x <= (colObject.x + colObject.w) &&
+            snowflake.y >= colObject.y && snowflake.y <= (colObject.y + colObject.h)) {
+            return true;
+        }
+        return false;
     }
 
     function randomBetween(min, max, round = false) {
         const result = Math.random() * (max - min) + min;
         return !round ? result : Math.floor(result);
     }
+
 })();
